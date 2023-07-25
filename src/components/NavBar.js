d2c4566b7fd8b91ab57faa1489b537ebdfedd251
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-25 07:41:07 +0300

    passes all the basic react routing tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const LinkStyles = {
+    margin: "10px",
+    textDecoration: "none",
+    backgroundColor: "coral",
+    color: "white",
+    padding: "5px 20px",
+    fontSize: "20px",
+  };
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        style={LinkStyles}
+        activeStyle={{
+          background: "#f0094d",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={LinkStyles}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={LinkStyles}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={LinkStyles}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
